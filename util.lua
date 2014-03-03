bc1ae88e4e459e1efd44c7ed1d7a773ae7c1210d
Alex <o0o.moro.o0o@gmail.com>
2014-03-03 04:51:45 -0700

    added 'tail' function to trim a table

--- util.lua
+++ util.lua
@@ -1,13 +1,30 @@
 require("string")
 
+util = {}
+
+-- returns the a table with only the tail of the integer keys.
+function util.tail(tbl)
+	local newtbl = {}
+	for i,val in ipairs(tbl) do
+		if i > 1 then
+			newtbl[i-1]=val
+		end
+	end
+	return newtbl
+end
 -- breaks string str into an array of words
-local function words(rest)
+function util.words(rest)
 	local words = {}
 	local i = 1 -- lua uses 1-indexed arrays
 	local idx=0
 
 	while idx do
-		idx = string.find(rest, " ")
+		if rest == nil then
+			idx = nil
+		else
+			idx = string.find(rest, " ")
+		end
+
 		if idx then
 			local word = string.sub(rest, 1, idx-1)
 			rest = string.sub(rest, idx+1)
@@ -21,3 +38,5 @@ local function words(rest)
 	end
 	return words
 end
+
+return util
