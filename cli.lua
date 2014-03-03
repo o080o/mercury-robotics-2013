abc3d9acbfe4189129f87309d4ed5b742f18d762
Alex <o0o.moro.o0o@gmail.com>
2014-03-03 05:48:29 -0700

    added coroutines to read output from server. io.read still blocks.

--- cli.lua
+++ cli.lua
@@ -1,6 +1,7 @@
 require("io")
 require("string")
 util = require("util")
+socketutil = require("socketutil")
 socket = require("socket")
 -- open the CLI interface.
 local commands = {}
@@ -30,6 +31,8 @@ function commands.connect(cmd, addr, port)
 		print("Connect failed")
 		return nil
 	end
+	-- new coroutine!
+	table.insert(co,coroutine.create(function () echoLoop(client) end))
 	print("Connected.")
 end
 
@@ -49,17 +52,45 @@ end
 -----------------------------------
 
 
--- main loop. read commands and lookup into function table.
-while running do
-	print("enter command:")
-	-- read one line from stdio
-	local cmdline = io.read()
-	local cmdwords = util.words(cmdline)
-	local cmd = cmdwords[1]
-	if commands[cmd] then
-		commands[cmd](unpack(cmdwords))
-	else
-		send(cmdline)
+-- listens and echos anything sent back from the server.
+function echoLoop(client)
+	local line
+	while running do
+		line = socketutil.receive(client,"*l")
+		if line then
+			print("Server:"..line)
+		end
 	end
 end
-	
+-- main loop. read commands and lookup into function table.
+function readLoop()
+	while running do
+		print("enter command:")
+		-- read one line from stdio
+		local cmdline = io.read()
+		local cmdwords = util.words(cmdline)
+		local cmd = cmdwords[1]
+		if commands[cmd] then
+			commands[cmd](unpack(cmdwords))
+		else
+			send(cmdline)
+		end
+		socket.sleep(1)
+		coroutine.yield()
+	end
+end
+
+-- coroutine scheduler.
+co = {}
+co["readLoop"] = coroutine.create(readLoop)	
+while running do
+	for k,c in pairs(co) do
+		if coroutine.status(c) == "dead" then
+			co[k]=nil
+			print(k,"dead")
+		else
+			status,err = coroutine.resume(c)
+			if not status then print(k,err) end
+		end
+	end
+end
